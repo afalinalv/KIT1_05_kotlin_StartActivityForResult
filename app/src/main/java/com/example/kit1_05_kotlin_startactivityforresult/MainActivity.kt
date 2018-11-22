94d8ee4d7965fe54971470ce4a826afe2f600b9e
afalinalv <afalinalv@ya.ru>
2018-11-22 03:11:05 +0300

    Second Activity for result AS 3.3.0-betta4 Gradle 4.10.1 Androidx Kotlin 1.3.10 API 28-22

---
+++ app/src/main/java/com/example/kit1_05_kotlin_startactivityforresult/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kit1_05_kotlin_startactivityforresult
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
